a9e07f852ee46c1b3a0aecedd5788fe3ebb8a732
EmreYildir90 <emreyildir@yahoo.com>
2026-09-08 10:46:24 -0400

    dasd

--- src/test/java/com/sample/utility/Driver.java
+++ src/test/java/com/sample/utility/Driver.java
@@ -15,24 +15,14 @@ public class Driver {
 
 
 
-    // Creating a private constructor, we are closing access to the
-    // object of this class from outside the class
+
     private Driver() {
     }
 
-    // We make WebDriver private, because we want to close access from outside of class
-    // We make it static, because we will use it inside static method
-    //
-    //private static WebDriver driver; // value is null by default
-
     private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
 
-
-    // Create a re-usable utility method which will return same driver instance when we call it
     public static WebDriver getDriver() {
 
-        // it will check if driver is null and if it is we will set up browser inside if statement
-        // if you already setup driver and using it again for following line of codes, it will return to same driver
         if (driverPool.get() == null) {
             String browserName = System.getProperty("browser") != null ? browserName = System.getProperty("browser") : ConfigurationReader.getProperty("browser");
 
@@ -86,11 +76,11 @@ public class Driver {
 
     }
 
-    // This method will make sure our driver value is always null after using quit() method
+
     public static void closeDriver(){
         if(driverPool.get() != null){
-            driverPool.get().quit(); // this line will terminate the existing driver session. with using this driver will not be even null
-            driverPool.remove();  //driver = null
+            driverPool.get().quit();
+            driverPool.remove();
         }
 
     }
